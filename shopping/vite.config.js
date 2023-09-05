3f2e404a0435f753a33b52492f33fbc7b2960582
Muhtadee Taron <sheikh.taron@northsouth.edu>
2023-09-06 02:42:18 +0600

    Update vite.config.js

--- shopping/vite.config.js
+++ shopping/vite.config.js
@@ -4,16 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRefresh()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+  resolve: {
+    alias: {
+      'react': 'https://cdn.skypack.dev/react@17',
+      'react-dom': 'https://cdn.skypack.dev/react-dom@17'
+    }
   },
+  plugins: [react(), reactRefresh()],
 })
