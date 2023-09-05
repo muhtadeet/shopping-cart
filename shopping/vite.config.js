216081261c4cfc6c67859a4557499c8e0259dd36
Muhtadee Taron <sheikh.taron@northsouth.edu>
2023-09-06 02:49:42 +0600

    experiments

--- shopping/vite.config.js
+++ shopping/vite.config.js
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
-import { splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-    },
-  },
-  plugins: [react(), splitVendorChunkPlugin()],
+  plugins: [react()],
 })
