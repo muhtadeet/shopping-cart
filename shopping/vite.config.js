48c058c1848b7ed632ed312251e321c3f8f2d9c2
Muhtadee Taron <sheikh.taron@northsouth.edu>
2023-09-06 02:47:53 +0600

    Update vite.config.js

--- shopping/vite.config.js
+++ shopping/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
-import reactRefresh from "@vitejs/plugin-react-refresh";
+import { splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      'react': 'https://cdn.skypack.dev/react@17',
-      'react-dom': 'https://cdn.skypack.dev/react-dom@17'
-    }
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/configuration-options/
+    },
   },
-  plugins: [react(), reactRefresh()],
+  plugins: [react(), splitVendorChunkPlugin()],
 })
