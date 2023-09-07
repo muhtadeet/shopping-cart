f238b10b4ebe02586bbae20a6d5738714f62aa28
Muhtadee Taron <sheikh.taron@northsouth.edu>
2023-09-07 18:25:48 +0600

    Update Data.jsx

--- shopping/src/components/Data.jsx
+++ shopping/src/components/Data.jsx
@@ -2,49 +2,49 @@ export const list = [
     {
       id: 1,
       title: "Orange",
-      img: "/images/fruit-1.jpeg",
+      img: "https://images.pexels.com/photos/2090902/pexels-photo-2090902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 5.50,
     },
     {
       id: 2,
       title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
+      img: "https://images.pexels.com/photos/7195228/pexels-photo-7195228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 3.00,
     },
     {
       id: 3,
       title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
+      img: "https://images.pexels.com/photos/326179/pexels-photo-326179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 10.00,
     },
     {
       id: 4,
       title: "Lemon",
-      img: "/images/fruit-4.jpeg",
+      img: "https://images.pexels.com/photos/1343537/pexels-photo-1343537.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 5.30,
     },
     {
       id: 5,
       title: "Avocado",
-      img: "/images/fruit-5.jpeg",
+      img: "https://images.pexels.com/photos/2228553/pexels-photo-2228553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 15.70,
     },
     {
       id: 6,
       title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
+      img: "https://images.pexels.com/photos/1002543/pexels-photo-1002543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 8.00,
     },
     {
       id: 7,
       title: "Banana",
-      img: "/images/fruit-7.jpeg",
+      img: "https://images.pexels.com/photos/2872755/pexels-photo-2872755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 7.50,
     },
     {
       id: 8,
       title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
+      img: "https://images.pexels.com/photos/9223283/pexels-photo-9223283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 12.20,
     },
   ];
@@ -53,25 +53,25 @@ export const list = [
     {
       id: 5,
       title: "Avocado",
-      img: "/images/fruit-5.jpeg",
+      img: "https://images.pexels.com/photos/2228553/pexels-photo-2228553.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 15.70,
     },
     {
       id: 6,
       title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
+      img: "https://images.pexels.com/photos/1002543/pexels-photo-1002543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 8.00,
     },
     {
       id: 7,
       title: "Banana",
-      img: "/images/fruit-7.jpeg",
+      img: "https://images.pexels.com/photos/2872755/pexels-photo-2872755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 7.50,
     },
     {
       id: 8,
       title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
+      img: "https://images.pexels.com/photos/9223283/pexels-photo-9223283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 12.20,
     },
   ];
@@ -80,19 +80,19 @@ export const list = [
     {
       id: 6,
       title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
+      img: "https://images.pexels.com/photos/1002543/pexels-photo-1002543.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 8.00,
     },
     {
       id: 7,
       title: "Banana",
-      img: "/images/fruit-7.jpeg",
+      img: "https://images.pexels.com/photos/2872755/pexels-photo-2872755.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 7.50,
     },
     {
       id: 8,
       title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
+      img: "https://images.pexels.com/photos/9223283/pexels-photo-9223283.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 12.20,
     },
   ];
@@ -101,19 +101,19 @@ export const list = [
     {
       id: 1,
       title: "Orange",
-      img: "/images/fruit-1.jpeg",
+      img: "https://images.pexels.com/photos/2090902/pexels-photo-2090902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 5.50,
     },
     {
       id: 2,
       title: "Tangerine",
-      img: "/images/fruit-2.jpeg",
+      img: "https://images.pexels.com/photos/7195228/pexels-photo-7195228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 3.00,
     },
     {
       id: 3,
       title: "Raspberry",
-      img: "/images/fruit-3.jpeg",
+      img: "https://images.pexels.com/photos/326179/pexels-photo-326179.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       price: 10.00,
     },
   ];
